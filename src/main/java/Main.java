ca0d8468501a9e1199c3cce1a3389183222aa285
ivan fomkin <ivan-fomkin@yandex.ru>
2020-02-03 20:17:56 +0300

    query rewrite

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,18 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox" +
-                "?serverTimezone=UTC";
+        String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "testtest";
         //Запрос на получение имени курса и среднего количества продаж в месяц
-        String query = "select course_name, datediff(MAX(subscription_date), MIN(subscription_date))/count(course_name)/30 as avg_purchase_per_month from purchaselist group by course_name;";
-
+        String query = "select course_name, count(subscription_date) / (select count(distinct MONTH(subscription_date))) as avg_value from purchaselist group by course_name order by course_name;";
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 String name = resultSet.getString("course_name");
-                String average_sale = resultSet.getString("avg_purchase_per_month");
+                String average_sale = resultSet.getString("avg_value");
                 System.out.println(name + " " + average_sale);
             }
 
